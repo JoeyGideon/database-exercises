b570ae338bb9fe00bcee4112019193db83ab4a03
Joey Gideon <joey.gideon1@gmail.com>
2023-03-09 15:33:25 -0600

    start

---
+++ limit_exercises.sql
@@ -0,0 +1,28 @@
+-- Q1
+show databases;
+use employees;
+select database();
+show tables;
+
+-- Q2   
+select distinct last_name
+from employees
+order by last_name desc
+limit 10;
+-- Q3
+ select first_name, last_name, birth_date, hire_date
+ from employees
+ where birth_date like ('%12-25')
+	and hire_date like ('199%')
+order by hire_date desc
+limit 5;
+-- Q3. Khun,Elvis,Sachar,Moto,Ulf
+
+-- Q4
+ select first_name, last_name, birth_date, hire_date
+ from employees
+ where birth_date like ('%12-25')
+	and hire_date like ('199%')
+order by hire_date desc
+limit 5 offset 45;
+    
