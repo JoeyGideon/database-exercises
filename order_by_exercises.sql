c247d688da1907b7f411128250de94b3a390345f
Joey Gideon <joey.gideon1@gmail.com>
2023-03-09 15:32:39 -0600

    update w/ q#s

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,25 +1,31 @@
+-- Q1
 show databases;
 use employees;
 select database();
 show tables;
 
+-- Q2
 select first_name, last_name, emp_no
 from employees
 where first_name in ('Irena','Vidya','Maya')
 order by first_name;
 -- Irena Reutenauer,Vidya Simmen
 
-
+-- Q3
 select first_name, last_name
 from employees
 where first_name in ('Irena','Vidya','Maya')
 order by first_name, last_name;
 -- Irena Acton, Vidya Zweizig
 
-select distinct last_name
+-- Q4
+select first_name, last_name, emp_no
 from employees
-where last_name like ('E%');
+where first_name in ('Irena','Vidya','Maya')
+order by last_name, first_name;
+-- Irena Acton,Maya Zyda
 
+-- Q5
 select distinct first_name, last_name, emp_no
 from employees
 where last_name like ('E%')
@@ -27,40 +33,21 @@ where last_name like ('E%')
 order by emp_no;
 -- 899, 10021, Ramazi Erde,Tadahiro Erde
     
+-- Q6    
 select distinct first_name, last_name, hire_date, birth_date
 from employees
 where last_name like ('E%')
 	and last_name not like ('%e')
 order by hire_date desc, birth_date asc;
 -- 1000, Phillip Eppinger, Oldest Vidar Eppinger
-select distinct last_name
 
-from employees
-where last_name like ('E%')
-	and last_name like ('%e');
- 
- select emp_no, hire_date, birth_date
- from employees
- where hire_date like ('199%');
- -- 10008,10011,10012
- 
- select emp_no, birth_date
- from employees
- where birth_date like ('%12-25');
- -- 10078,10115,10261
- 
+ -- Q7
  select first_name, last_name, birth_date, hire_date
  from employees
  where birth_date like ('%12-25')
 	and hire_date like ('199%')
 order by birth_date asc, hire_date desc;
-    -- 362, Khun Bernini, Douadi Pettis 
+    -- 362,Khun Bernini,Dauadi Pettis
     
-select distinct last_name
-from employees
-where last_name like ('%q%');
 
-select distinct last_name
-from employees
-where last_name like ('%q%')
-	and last_name not like ('%qu%');
+
