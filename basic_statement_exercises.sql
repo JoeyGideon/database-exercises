ed990dff230c7bb6e4de69efaf733542d6685c6b
Joey Gideon <joey.gideon1@gmail.com>
2023-03-09 10:42:52 -0600

    reviewed

--- basic_statement_exercises.sql
+++ basic_statement_exercises.sql
@@ -6,30 +6,31 @@ show create table albums;
  
 --  PRIMARY KEY (`id`)
 -- `name` varchar(240) NOT NULL, the album name
--- `sales` float DEFAULT NULL, total earnings from the album sales
+-- `sales` float DEFAULT NULL, earings could be money or number sold?
 
 select *
 from albums
 where artist = 'Pink Floyd';
 -- The Dark Side of the Moon, The Wall
 
-select *
+select release_date, name
 from albums
 where name = "Sgt. Pepper's Lonely Hearts Club Band";
 -- 1967
-select *
+
+select genre
 from albums
 where name = 'Nevermind';
 -- Grunge,Alternative Rock
 
-select *
+select name, release_date
 from albums
 where release_date between 1990 and 1999;
 
 /*The Bodyguard,Jagged Little Pill,Come On Over,Falling into You,Let's talk about love,Dangerous,the immaculate collections,
 Titanic: Muscic from the Motion Picture,Metallica,Nevermind,Supernatural*/
 
-select *
+select name, sales
 from albums
 where sales < 20;
 
