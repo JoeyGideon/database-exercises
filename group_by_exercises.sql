5e94324696fc3a61d3f857acda6930f44e429db3
Joey Gideon <joey.gideon1@gmail.com>
2023-03-10 16:52:08 -0600

    reviewed

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -9,7 +9,7 @@ from titles;
 -- 7
 
 -- Q3 
-select distinct last_name
+select last_name
 from employees
 where last_name like 'e%e'
 group by last_name;
@@ -18,7 +18,7 @@ group by last_name;
 select distinct first_name, last_name
 from employees
 where last_name like 'e%e'
-	and first_name like 'e%e';
+group by first_name, last_name;
 
 -- Q5
 select distinct last_name
@@ -27,41 +27,53 @@ where last_name like '%q%'
 	and last_name not like '%qu%';
     
 -- Q6
-select count(distinct last_name)
+select last_name, count(*)
 from employees
 where last_name like '%q%'
-	and last_name not like '%qu%';
-    
+	and last_name not like '%qu%'
+group by last_name;
+-- Chleq, Lindqvist, Qiwen
 -- Q7 
-select gender, count(gender)
+select first_name, gender, count(gender)
 from employees
-where first_name = 'Irena'
-	or first_name = 'Vidya'
-	or first_name = 'Maya'
-group by gender; 
+where first_name IN ( 'Irena', 'Vidya', 'Maya')	
+group by first_name, gender; 
 
 -- Q8
-select count( lower(concat(substr(first_name,1,1),
+select lower(concat(substr(first_name,1,1),
 substr(last_name,1,4), "_" ,substr(birth_date,6,2),
-substr(birth_date,3,2)))) as username
+substr(birth_date,3,2))) as username, count(*)
 from employees
+group by username
 ;
 
 -- Q9 
+
 select lower(concat(substr(first_name,1,1),
 substr(last_name,1,4), "_" ,substr(birth_date,6,2),
-substr(birth_date,3,2))) as username, count(*) as duplicate
+substr(birth_date,3,2))) as username, count(*)
 from employees
 group by username
-order by duplicate desc
+having count(*) > 1
+order by count(*) DESC
 ;
 -- Bonus: 13251 having duplicate > 1
+select count(*)
+from
+	(select lower(concat(substr(first_name,1,1),
+substr(last_name,1,4), "_" ,substr(birth_date,6,2),
+substr(birth_date,3,2))) as username, count(*)
+from employees
+group by username
+having count(*) > 1
+order by count(*) DESC) as a;
 
 -- Bonus Questions
 
-select round(avg(salary),2), emp_no
+select emp_no, round(avg(salary),2)
 from salaries
-group by emp_no;
+group by emp_no
+;
 
 select count(emp_no), dept_no
 from dept_emp
@@ -81,8 +93,8 @@ group by emp_no;
 
 select max(salary), emp_no
 from salaries
-where salary > '150000'
-group by emp_no;
+group by emp_no
+having max(salary) > '150000';
 
 select round(avg(salary), 2) , emp_no
 from salaries
